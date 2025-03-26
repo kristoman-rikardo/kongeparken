5f40fc6f7923ebf77074f1c9353e8f455ffa7c90
kristoman-rikardo <kristofferswik@gmail.com>
2025-03-26 10:57:20 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,5 +1,4 @@
 //Denne koden tilhører Dalai Solutions AS, og er på leie til Kongeparken AS. Bruk uten tillatelse er ikke tillatt.
-<script type="text/javascript">
   (function(d, t) {
       var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
       v.onload = function() {
@@ -14,4 +13,3 @@
       }
       v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
   })(document, 'script');
-</script>
