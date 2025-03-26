c24cbb548514c767ad9e14f366f914cd6eba62f1
kristoman-rikardo <kristofferswik@gmail.com>
2025-03-26 10:56:57 +0100

    Update console.js

--- console.js
+++ console.js
@@ -1,26 +1,17 @@
- let proactiveMessage = "Hei, jeg hjelper deg gjerne!🐻";
-    let widgetStyleSheet = "https://kristoman-rikardo.github.io/kongeparken/style.css";
-
-    (function(d, t) {
+//Denne koden tilhører Dalai Solutions AS, og er på leie til Kongeparken AS. Bruk uten tillatelse er ikke tillatt.
+<script type="text/javascript">
+  (function(d, t) {
       var v = d.createElement(t), s = d.getElementsByTagName(t)[0];
       v.onload = function() {
         window.voiceflow.chat.load({
-          verify: { projectID: '6788e62fd604871f7c5764d9' },
+          verify: { projectID: '67e1d5a5eb295306cbdbca4d' },
           url: 'https://general-runtime.voiceflow.com',
           versionID: 'production',
-          allowDangerousHTML: true,
-          assistant: {
-            stylesheet: widgetStyleSheet
+          voice: {
+            url: "https://runtime-api.voiceflow.com"
           }
-        }).then(() => {
-          console.log('Voiceflow widget loaded successfully');
-          window.voiceflow.chat.proactive.clear();
-          window.voiceflow.chat.proactive.push(
-            { type: 'text', payload: { message: proactiveMessage } }
-          );
-        }).catch((err) => console.error('Widget load failed:', err));
-      };
-      v.src = "https://cdn.voiceflow.com/widget/bundle.mjs";
-      v.type = "text/javascript";
-      s.parentNode.insertBefore(v, s);
-    })(document, 'script');
+        });
+      }
+      v.src = "https://cdn.voiceflow.com/widget-next/bundle.mjs"; v.type = "text/javascript"; s.parentNode.insertBefore(v, s);
+  })(document, 'script');
+</script>
